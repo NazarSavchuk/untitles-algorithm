7649ffabff1d8cb49f8be760dcfc2973f4505283
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-09-10 12:49:29 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -24,32 +24,82 @@ const path = require("path");
 })();
 
 function count(users, mobileDevices, iotDevices) {
-  const mobileDeviceCount = fromIotToMobile(mobileDevices, iotDevices);
-  return fromMobileToUser(users, mobileDevices, mobileDeviceCount);
+  return fromMobileToUser(users, mobileDevices, iotDevices);
+  // expected time complexity O(n, m, c) = n * m + m * c + n
+  //----------------------------------------------------
+
+  // return firstIdea (users, mobileDevices, iotDevices)
+
+  // array with count of iotDevices of every unique user
+  // expected time complexity O(n, m, c) = n * m * c    **bad choice**
 }
 
-function fromMobileToUser(users, mobileDevices, mobileDeviceCount) {
-  let result = [];
+function fromMobileToUser(users, mobileDevices, iotDevices) {
+  const mobileDeviceCount = fromIotToMobile(mobileDevices, iotDevices);
+
+  function fromIotToMobile(mobileDevices, iotDevices) {
+    const mobileDeviceCount = Array(mobileDevices.length).fill(0);
+
+    for (let i = mobileDevices.length; i > 0; i--) {
+      for (let j = iotDevices.length; j > 0; j--) {
+        if (mobileDevices[i - 1].id === iotDevices[j - 1].mobile) {
+          mobileDeviceCount[i - 1]++;
+          iotDevices.splice(j - 1, 1);
+        }
+      }
+    }
+
+    return mobileDeviceCount;
+  }
+
+  const usersResult = users.map((user) => user.name.split(" ")[0]);
+  const result = Array(users.length).fill(0);
+
+  for (let i = users.length; i > 0; i--) {
+    for (let j = mobileDevices.length; j > 0; j--) {
+      if (users[i - 1].id === mobileDevices[j - 1].user) {
+        result[i - 1] = result[i - 1] + mobileDeviceCount[j - 1];
+        mobileDevices.splice(j - 1, 1);
+        mobileDeviceCount.splice(j - 1, 1);
+      }
+    }
+    result[i - 1] = {
+      user: usersResult[i - 1],
+      devices: result[i - 1],
+    };
+  }
+
+  function findDuplicates(result) {
+    for (let i = 0; i < result.length; i++) {
+      //find duplicates in users
+      const tempIndex = result.findIndex(
+        (user) => user.user === result[i].user
+      );
+      if (tempIndex !== i) {
+        result[tempIndex].devices += result[i].devices;
+        result.splice(i, 1);
+        i--;
+      }
+    }
+    return result;
+  }
+
+  return findDuplicates(result);
+}
+
+function firstIdea(users, mobileDevices, iotDevices) {
+  const resU = Array(users.length).fill(0);
   for (let i = 0; i < users.length; i++) {
     for (let j = 0; j < mobileDevices.length; j++) {
-      if (mobileDevices[j].user === users[i].id) {
-        console.log("");
+      for (let k = 0; k < iotDevices.length; k++) {
+        if (
+          users[i].id === mobileDevices[j].user &&
+          mobileDevices[j].id === iotDevices[k].mobile
+        ) {
+          resU[i]++;
+        }
       }
     }
   }
-}
-
-function fromIotToMobile(mobileDevices, iotDevices) {
-  const mobileDeviceCount = Array(mobileDevices.length).fill(0);
-
-  for (let i = 0; i < mobileDevices.length; i++) {
-    for (let j = 0; j < iotDevices.length + 2; j++) {
-      if (mobileDevices[i].id === iotDevices[0].mobile) {
-        mobileDeviceCount[i]++;
-        iotDevices.shift();
-      }
-    }
-  }
-
-  return mobileDeviceCount;
+  return resU;
 }
